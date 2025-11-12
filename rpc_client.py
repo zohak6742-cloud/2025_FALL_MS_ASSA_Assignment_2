f0fd1427681dbdde5638b3614261d7e7a40da899
zohak6742-cloud <zohak6742@gmail.com>
2025-11-12 21:44:06 +0500

    Delete rpc_client.py

--- rpc_client.py
+++
@@ -1,9 +0,0 @@
-import xmlrpc.client
-
-
-proxy = xmlrpc.client.ServerProxy("http://localhost:8000/")
-
-print("Requesting remote method...")
-name = input("Enter your name: ")
-response = proxy.greet(name)
-print("Response from Server:", response)
